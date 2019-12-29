1143a57ff48884365b8e05c0ea5a8ef52021cfa4
Vasudevatirupathinaidu <tirupathinaidu07@gmail.com>
2019-12-29 20:55:49 +0530

    Numbers

---
+++ Chapter_02 Variables and Data Types/number.py
@@ -0,0 +1,52 @@
+# Numbers
+
+# Integers
+print(2 + 3) # add
+print(3 - 2) # subtract
+print(2 * 3) # multiply
+print(3 / 2) # divide
+print(3 % 2) # modulo
+print(3 ** 2) # exponents
+
+
+# Order of operations - BI(O)DMAS (Brackets, Indices(Order or Other), Division, Multiplication, Addition, Subtraction)
+print(2 + 3 * 4)
+print((2 + 3) * 4)
+
+
+# Floats
+print(0.1 + 0.1)
+print(0.2 + 0.2)
+print(2 * 0.1)
+print(2 * 0.2)
+
+
+# Strange case
+print(0.2 + 0.1)
+print(3 * 0.1)
+
+
+# Integers and Floats
+print(4/2)
+print(1 + 2.0)
+print(2 * 3.0)
+print(3.0 ** 2)
+
+
+# Underscores in numbers
+print(2_3)
+universe_age = 14_000_000_000
+print(universe_age) # Python ignores the underscores and prints only the digits - This feature is only available in Python 3.6 and later
+print(1_000)
+print(10_00)
+
+
+# Multiple Assignment
+x, y, z= 1, 2, 3
+print(x, y, z)
+
+
+# Constant - Python doesn't have built in const types but python programmers use all capital letters to indicate a variable should be treated as a constant and never be changed
+
+MAX_CONNECTIONS = 5000
+print(MAX_CONNECTIONS)
