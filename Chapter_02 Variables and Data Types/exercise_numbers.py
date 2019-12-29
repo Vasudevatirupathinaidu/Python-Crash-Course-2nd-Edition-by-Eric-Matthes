8f3e984ec4dec525d866e6e70c6cf32e28cadfea
Vasudevatirupathinaidu <tirupathinaidu07@gmail.com>
2019-12-29 20:59:56 +0530

    Exercise_numbers

---
+++ Chapter_02 Variables and Data Types/exercise_numbers.py
@@ -0,0 +1,10 @@
+# Number eight
+print(5 + 3)
+print(10 - 2)
+print(4 * 2)
+print(24 / 3)
+
+# Favorite number
+favorite_number = 1.618
+message = f"My favorite number is {favorite_number}"
+print(message)
