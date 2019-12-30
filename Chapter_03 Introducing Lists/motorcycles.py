d1faaaf10ccd95b9bfe0f75d96ad31590653bd01
Vasudevatirupathinaidu <tirupathinaidu07@gmail.com>
2019-12-30 19:24:18 +0530

    Organizing a List

--- Chapter_03 Introducing Lists/motorcycles.py
+++ Chapter_03 Introducing Lists/motorcycles.py
@@ -71,3 +71,50 @@ too_expensive = 'ducati'
 motorcycles.remove(too_expensive)
 print(motorcycles)
 print(f"\nA {too_expensive.title()} is too expensive for me.")
+
+
+
+# Organizing a List
+print("\n\n\n")
+print("Organizing a List")
+cars = ['bmw','audi', 'toyota', 'subaru']
+print(cars)
+
+# Sorting a List Permanently with the sort() method
+print("\n\n\n")
+cars = ['bmw','audi', 'toyota', 'subaru']
+cars.sort() # alphabetical order and we never revert to the original order
+print(cars)
+
+# reverse alphabetical order
+cars.sort(reverse = True) # the order of the list is permanently changed
+print(cars)
+
+# Sorting a List Temporarily with the sorted() function
+print("\n\n\n")
+cars = ['bmw','audi', 'toyota', 'subaru']
+print("Here is the original list:")
+print(cars)
+print("\nHere is the sorted list:")
+print(sorted(cars))
+print("\nHere is the original list again:")
+print(cars)
+
+# alphabetical order - Temporary
+print(sorted(cars))
+# reverse alphabetical order - Temporary
+print(sorted(cars, reverse = True))
+
+
+# Printing a List in Reverse Order
+print("\n\n\n")
+cars = ['bmw','audi', 'toyota', 'subaru']
+print(cars)
+cars.reverse() # This method changes the order of a list permanently
+print(cars)
+
+
+# Finding the Length of a List
+print("\n\n\n")
+cars = ['bmw','audi', 'toyota', 'subaru']
+print(len(cars))
