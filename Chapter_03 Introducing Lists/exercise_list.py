f7d930dfc04c8101fc81536f6186834cd61ed4b9
Vasudevatirupathinaidu <tirupathinaidu07@gmail.com>
2019-12-30 12:39:43 +0530

    Exercise_List

---
+++ Chapter_03 Introducing Lists/exercise_list.py
@@ -0,0 +1,21 @@
+# Names
+names = ['vasu', 'deva', 'tirupathi', 'naidu']
+print(names)
+print(names[0])
+print(names[1])
+print(names[2])
+print(names[3])
+
+
+# Greetings
+print(f"Hello, {names[0]}! Good to see you again.")
+print(f"Hello, {names[1]}! Good to see you again.")
+print(f"Hello, {names[2]}! Good to see you again.")
+print(f"Hello, {names[3]}! Good to see you again.")
+
+
+# Your Own List
+my_list = ['Prestige', 'Inception', 'The Dark Knight']
+print(f"{my_list[0]}: Are you watching closely?")
+print(f"{my_list[1]}: You mustn't be afraid to dream a little bigger darling")
+print(f"{my_list[2]}: Why So Serious?")
