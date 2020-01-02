caf8d932aab563086f6adf67b9134f94e98901c8
Vasudevatirupathinaidu <tirupathinaidu07@gmail.com>
2020-01-02 18:02:22 +0530

    Testing multiple conditions

--- Chapter_05 If statements/toppings.py
+++ Chapter_05 If statements/toppings.py
@@ -1,4 +1,16 @@
 # Checking for inequality
 requested_topping = 'mushrooms'
 if requested_topping != 'anchovies':
-    print("Hold the anchovies!")
+    print("Hold the anchovies!\n")
+
+
+# Testing multiple conditions
+requested_toppings = ['mushrooms', 'extra cheese']
+if 'mushrooms' in requested_toppings:
+    print("Adding mushrooms.")
+if 'pepperoni' in requested_toppings:
+    print("Adding pepperoni.")
+if 'extra cheese' in requested_toppings:
+    print("Adding extra cheese.")
+
+print("\nFinished making your pizza!")
