90a37fcc38eb87a305cee980461c39ce137c1e27
Vasudevatirupathinaidu <tirupathinaidu07@gmail.com>
2020-01-02 18:02:49 +0530

    if-else statements

---
+++ Chapter_05 If statements/voting.py
@@ -0,0 +1,20 @@
+# if statements
+
+# Simple if statements
+# if conditional_test:
+#     do something
+
+age = 19
+if age >= 18:
+    print("You are older enough to vote!")
+    print("Have you registered to vote yet?\n")
+
+
+# if-else statements
+age = 17
+if age >= 18:
+    print("You are older enough to vote!")
+    print("Have you registered to vote yet?\n")
+else:
+    print("Sorry, you are too young to vote.")
+    print("Please register to vote as soon as you turn 18!\n")
