657cdef808a6cc54109874ad5db2d63348dc0f30
Vasudevatirupathinaidu <tirupathinaidu07@gmail.com>
2020-01-03 00:01:48 +0530

    multiple lists

--- Chapter_05 If statements/exercise.py
+++ Chapter_05 If statements/exercise.py
@@ -48,3 +48,117 @@ print(8 in numbers)
 print(3 in numbers)
 print(7 not in numbers)
 print(6 not in numbers)
+print("\n")
+
+
+# Alien colors #1
+alien_color = 'green'
+if alien_color == 'green':
+    print("The player just earned 5 points.\n")
+
+alien_color = 'yellow'
+if alien_color == 'green':
+    print("The player just earned 5 points.")
+
+# Alien colors #2
+alien_color = 'green'
+if alien_color == 'green':
+    print("The player just earned 5 points.\n")
+else:
+    print("The player just earned 10 points.\n")
+
+alien_color = 'red'
+if alien_color == 'green':
+    print("The player just earned 5 points.\n")
+else:
+    print("The player just earned 10 points.\n")
+
+
+# Alien colors #3
+
+alien_color = 'red'
+
+if alien_color == 'green':
+    points = 5
+elif alien_color == 'yellow':
+    points = 10
+elif alien_color == 'red':
+    points = 15
+else:
+    print("")
+
+print(f"The player just earned {points} points.\n")
+
+
+# Stages of life
+age = 4
+
+if age < 2:
+    person = 'baby'
+elif age >= 2 and age < 4:
+    person = 'toddler'
+elif age >= 4 and age < 13:
+    person = 'kid'
+elif age >= 13 and age < 20:
+    person = 'teenager'
+elif age >= 20 and age < 65:
+    person = 'adult'
+    print(f"The person is an {person}")
+else:
+    person = 'elder'
+    print(f"The person is an {person}")
+
+print(f"The person is a {person}\n")
+
+
+# Favorite fruit
+
+favorite_fruits = ['guava', 'apple', 'banana']
+
+if 'guava' in favorite_fruits:
+    print("My favorite fruit is guava.")
+if 'apple' in favorite_fruits:
+    print("My favorite fruit is apple.")
+if 'banana' in favorite_fruits:
+    print("My favorite fruit is banana.")
+if 'jackfruit' in favorite_fruits:
+    print("My favorite fruit is jackfruit.")
+if 'orange' in favorite_fruits:
+    print("My favorite fruit is orange.")
+
+print("\n")
+
+
+# Hello Admin
+usernames = ['ching', 'chang', 'admin', 'chung', 'ding']
+for username in usernames:
+    if username == 'admin':
+        print("Hello admin, would you like to see a status report?")
+    else:
+        print(f"Hello {username}, thank you for logging in again.")
+
+print("\n")
+
+
+# No users
+
+usernames = []
+
+if usernames:
+    for username in usernames:
+        print(f"Hello {username}, thank you for logging in again.")
+else:
+    print("We need to find some users!")
+
+print("\n")
+
+
+# Checking usernames
+
+current_users = ['ching', 'chang', 'chung', 'ding', 'bing']
+new_users = ['admin', 'magnus', 'ching', 'carlsen', 'ding']
+for new_user in new_users:
+    if new_user in current_users:
+        print(f"Username {new_user} is available.\n")
+    if new_user not in current_users:
+        print(f"{new_user}, You will need to enter a new username.\n")
