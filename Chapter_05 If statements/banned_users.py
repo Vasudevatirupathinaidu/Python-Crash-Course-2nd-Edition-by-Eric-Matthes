1aa806d5591695719122ad4cc3cb2a93fe793803
Vasudevatirupathinaidu <tirupathinaidu07@gmail.com>
2020-01-02 15:57:01 +0530

    A value is not in a list

---
+++ Chapter_05 If statements/banned_users.py
@@ -0,0 +1,11 @@
+# Checking whether a value is Not in a list
+banned_users = ['andrew', 'carolina', 'david']
+user = 'marie'
+
+if user not in banned_users:
+    print(f"{user.title()}, you can post a response if you wish.")
+
+# Boolean expression - another name for conditional test.
+# Boolean value is either True or False
+game_active = True
+can_edit = False
