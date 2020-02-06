d7aae939387d6ee6385796c071f394be7e9a7b4b
Vasudevatirupathinaidu <tirupathinaidu07@gmail.com>
2020-02-06 16:23:20 +0530

    exercise

---
+++ Chapter_07 User Input and While Loops/exercise.py
@@ -0,0 +1,23 @@
+# Rental Car
+# users_car_name = input("What kind of rental car you would like to ride? ")
+# print(f"\nLet me see if i can find you a {users_car_name.title()}.")
+
+
+# Restaurant Seating
+# number_of_people = input("How many people are in your dinner group? ")
+# number_of_people = int(number_of_people)
+
+# if number_of_people > 8:
+#     print("\nPlease wait for sometime for a dinner table.")
+# else:
+#     print("\nYour table is ready. Please join us.")
+
+
+# Multiples of Ten
+number = input("Please a enter a number: ")
+number = int(number)
+if number % 10 == 0:
+    print(f"{number} is a multiple of 10.")
+else:
+    print(f"{number} is not a multiple of 10.")
+    
