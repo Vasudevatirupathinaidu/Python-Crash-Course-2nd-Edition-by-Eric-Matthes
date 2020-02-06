8616b9ac9ac52870ce6452da7f0539ab45948f5f
Vasudevatirupathinaidu <tirupathinaidu07@gmail.com>
2020-02-06 19:54:49 +0530

    flag

--- Chapter_07 User Input and While Loops/parrot.py
+++ Chapter_07 User Input and While Loops/parrot.py
@@ -1,3 +1,13 @@
-# User Input and While Loops
-message = input("Tell me something, and I will repeat it back to you: ")
-print(message)
+# flag - For a program that should run only as long as many conditions are true, you can define one variable that determines whether or not the entire program is active. This variable, called a flag, acts as a signal to the program.
+
+prompt = "\nTell me something, and I will repeat it back to you:"
+prompt += "\nEnter 'quit' to end the program. "
+
+active = True # here active variable is a flag
+
+while active:
+    message = input(prompt)
+    if message == 'quit':
+        active = False
+    else: 
+        print(message)
