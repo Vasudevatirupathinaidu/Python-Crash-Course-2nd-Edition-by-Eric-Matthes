5c92f7fa0ceae7293cec97459a5429a3874e28fc
Vasudevatirupathinaidu <tirupathinaidu07@gmail.com>
2020-02-10 23:23:40 +0530

    exercise

--- Chapter_08 Functions/exercise.py
+++ Chapter_08 Functions/exercise.py
@@ -2,8 +2,25 @@
 def display_message():
     print("Hello Everyone! In this chapter we are going to learn about a function.")
 display_message()
+print("\n")
+
 
 # Favorite Book
 def favorite_book(title, author):
     print(f"My favorite book is {title.title()} by {author.title()}.")
 favorite_book('thinking in numbers', 'daniel tammet')
+print("\n")
+
+
+# # T-Shirt
+# def make_shirt(size, message):
+#     print(f"My chest size of the shirt is {size} inches.")
+# make_shirt(45)
+# make_shirt(size=45)
+# print("\n")
+
+# # Large Shirts
+# def make_Shirt(size='large'):
+#     if size == 'large':
+#         print('I Love Python')
+#     elif size == 'medium':
