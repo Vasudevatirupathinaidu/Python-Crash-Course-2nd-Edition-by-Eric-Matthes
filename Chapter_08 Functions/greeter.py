95f000df4d994c912f0c78c6b84581d5eef200de
Vasudevatirupathinaidu <tirupathinaidu07@gmail.com>
2020-02-10 20:13:12 +0530

    def function

---
+++ Chapter_08 Functions/greeter.py
@@ -0,0 +1,15 @@
+# Defining a Function
+def greet_user():
+    """Display a simple greeting.""" # docstring - Which describes what the function does.
+    print("Hello Python Lovers!")
+
+greet_user()
+
+
+# Passing information to a function
+def greet_user(username):
+    """Display a simple greeting."""
+    print(f"Hello, {username.title()}!")
+
+greet_user('deva')
+greet_user('vasu')
