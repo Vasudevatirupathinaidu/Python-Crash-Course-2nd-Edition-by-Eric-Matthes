65946df023e8c1b0b1dc813d98dd71f19a42b0a0
Vasudevatirupathinaidu <tirupathinaidu07@gmail.com>
2020-02-10 20:24:55 +0530

    exercise

---
+++ Chapter_08 Functions/exercise.py
@@ -0,0 +1,9 @@
+# Message
+def display_message():
+    print("Hello Everyone! In this chapter we are going to learn about a function.")
+display_message()
+
+# Favorite Book
+def favorite_book(title, author):
+    print(f"My favorite book is {title.title()} by {author.title()}.")
+favorite_book('thinking in numbers', 'daniel tammet')
