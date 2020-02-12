06b4b274fae642c28a74e27a0794623cd4dc6446
Vasudevatirupathinaidu <tirupathinaidu07@gmail.com>
2020-02-12 12:38:55 +0530

    Passing a list

---
+++ Chapter_08 Functions/greet_users.py
@@ -0,0 +1,8 @@
+# Passing a List
+def greet_users(names):
+    """Print a simple greeting to each user in the list."""
+    for name in names:
+        print(f"Hello, {name.title()}!")
+
+usernames = ['vasu', 'deva', 'tiru']
+greet_users(usernames)
