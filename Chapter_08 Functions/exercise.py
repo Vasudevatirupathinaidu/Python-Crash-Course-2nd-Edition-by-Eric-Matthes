32eddaa1f854b8505ed853af29b4b5914f53a7b0
Vasudevatirupathinaidu <tirupathinaidu07@gmail.com>
2020-02-12 12:38:34 +0530

    exercise

--- Chapter_08 Functions/exercise.py
+++ Chapter_08 Functions/exercise.py
@@ -20,6 +20,7 @@ make_shirt('large', 'I Love Python')
 make_shirt(message="I prefer medium size t-shirt", size='medium')
 print("\n")
 
+
 # Large Shirts
 def make_shirt(size='large', message="I Love Python"):
     print(f"\nI am going to make a {size} t-shirt.")
@@ -29,6 +30,7 @@ make_shirt(size='medium')
 make_shirt('small', 'Long time no see')
 print("\n")
 
+
 # Cities
 def describe_city(city, country = 'India'):
     print(f"\n{city} is in {country}.")
@@ -36,4 +38,69 @@ def describe_city(city, country = 'India'):
 describe_city('Vizag')
 describe_city(city = 'Bangalore')
 describe_city(city = 'California', country = 'USA')
+print("\n")
 
+
+# City Names
+def city_country(city, country):
+    print(f'\n"{city}, {country}"')
+
+city_country(city='Rajahmundry', country='India')
+city_country('Vizag', 'India')
+city_country(city='California', country='India')
+print("\n")
+
+
+# Album
+def make_album(artist_name, album_title, no_of_songs=None):
+    artist = dict()
+
+    if no_of_songs:
+        artist['Songs'] = no_of_songs
+
+    artist['Artist'] = artist_name
+    artist['Album'] = album_title
+
+    return f"\n{artist}"
+
+# Take inputs from users
+name = input(f"\nPlease enter artist name: ")
+album = input(f"\nPlease enter album title: ")
+songcount = input("\nNumber of songs are in this album: ")
+
+fav_album = make_album(name, album, songcount)
+print(fav_album)
+
+
+# User Albums
+def make_album(artist_name, album_title, no_of_songs=None):
+    artist = dict()
+
+    artist['Artist'] = artist_name
+    artist['Album'] = album_title
+
+    if no_of_songs:
+        artist['Songs'] = no_of_songs
+
+    return f"\n{artist}"
+
+# Take inputs from users
+while True:
+    print("\nPlease enter below details: ")
+    print("Type 'q' to quit this task.")
+
+    name = input("\nPlease enter artist name: ")
+    if name == 'q':
+        break
+
+    album = input("\nPlease enter album title: ")
+    if album == 'q':
+        break
+
+    songcount = input("\nNumber of songs are in this album: ")
+    if songcount == 'q':
+        break
+
+    fav_album = make_album(name, album, songcount)
+
+    print(fav_album)
