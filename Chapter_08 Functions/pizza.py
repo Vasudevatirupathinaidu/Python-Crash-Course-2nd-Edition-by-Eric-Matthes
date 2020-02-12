2c4cc3c462127ab5cb0d70e82281d3f99358ea70
Vasudevatirupathinaidu <tirupathinaidu07@gmail.com>
2020-02-12 17:18:44 +0530

    Mixing Positional and Arbitrary Arguments

--- Chapter_08 Functions/pizza.py
+++ Chapter_08 Functions/pizza.py
@@ -1,22 +1,22 @@
 # Passing a Arbitary Number of Arguments
-def make_pizza(*toppings):
-    """Print the list of toppings that have been requested"""
-    print(toppings)
+# def make_pizza(*toppings):
+#     """Print the list of toppings that have been requested"""
+#     print(toppings)
 
-make_pizza('pepperoni')
-make_pizza('mushrooms', 'green peppers', 'extra cheese')
-print("\n")
+# make_pizza('pepperoni')
+# make_pizza('mushrooms', 'green peppers', 'extra cheese')
+# print("\n")
 
 
-def make_pizza(*toppings):
-    """Summerize the pizza we are about to make."""
-    print("\nMaking a pizza with the following toppings:")
-    for topping in toppings:
-        print(f"- {topping}")
+# def make_pizza(*toppings):
+#     """Summerize the pizza we are about to make."""
+#     print("\nMaking a pizza with the following toppings:")
+#     for topping in toppings:
+#         print(f"- {topping}")
 
-make_pizza('pepperoni')
-make_pizza('mushrooms', 'green peppers', 'extra cheese')
-print("\n")
+# make_pizza('pepperoni')
+# make_pizza('mushrooms', 'green peppers', 'extra cheese')
+# print("\n")
 
 
 # Mixing Positional and Arbitrary Arguments
@@ -25,7 +25,3 @@ def make_pizza(size, *toppings):
     print(f"\nMaking a {size}-inch pizza with the following toppings:")
     for topping in toppings:
         print(f"- {topping}")
-
-make_pizza(16, 'pepperoni')
-make_pizza(12, 'mushrooms', 'green peppers', 'extra cheese')
-print("\n")
