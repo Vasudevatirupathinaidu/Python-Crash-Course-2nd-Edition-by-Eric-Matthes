ef19af08e71a0a92aa7c0993ada2a0f98fddd143
Vasudevatirupathinaidu <tirupathinaidu07@gmail.com>
2020-02-16 16:13:10 +0530

    correction

--- Chapter_08 Functions/person.py
+++ Chapter_08 Functions/person.py
@@ -7,6 +7,13 @@ print(musician)
 print("\n")
 
 
+import person_functions as pf
+
+musician = pf.bulid_person('Vasudev', 'Bonu', age = 27)
+print(musician)
+print("\n")
+
+
 from person_functions import bulid_person
 
 musician = bulid_person('Vasudev', 'Bonu', age = 27)
@@ -21,15 +28,8 @@ print(musician)
 print("\n")
 
 
-import person_functions as pf
-
-musician = pf.bulid_person('Vasudev', 'Bonu', age = 27)
-print(musician)
-print("\n")
-
-
 from person_functions import *
 
-musician = person_functions.bulid_person('Vasudev', 'Bonu', age = 27)
+musician = bulid_person('Vasudev', 'Bonu', age = 27)
 print(musician)
 print("\n")
