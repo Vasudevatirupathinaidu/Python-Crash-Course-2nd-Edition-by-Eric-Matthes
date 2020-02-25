fb382e6c2781c3626e797e667f9c13aec5f1b876
Vasudevatirupathinaidu <tirupathinaidu07@gmail.com>
2020-02-25 11:36:43 +0530

    comparison - ord() function

--- All.py
+++ All.py
@@ -733,7 +733,20 @@ print("\n")
 # Less or Equal: <=
 # Object Identity: is
 
+print(10 > 3)
+print(10 >= 3)
+print(10 < 20)
+print(10 <= 20)
+print(10 == 10)
+print(10 == "10")
+print(10 != "10")
 
+print("bag" > "apple")
+print("bag" > "BAG")
+print("bag" == "BAG")
+print(ord("b"))
+print(ord("B"))
+print("\n")
 
 language = 'python'
 
@@ -1150,8 +1163,10 @@ print("\n")
 
 # hello_func()
 
+name = 'deva'
+
 def hello_func():
-    return "Hello Function."
+    return f"Hello {name.title()}"
 
 print(hello_func())
 print(hello_func())
